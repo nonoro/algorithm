d2357da5b86987ce519d94cdddf02b412e09038f
nonoro <kgu1022@naver.com>
2023-02-02 16:38:45 +0900

    완전탐색
    - 오름차순
    - 중복허용안됨
    - 앞뒤 바꾼 숫자도 허용안됨.

---
+++ src/brute_force/_15650_오름차순이며_중복허용이안되고_앞뒤바꾼숫자도_허용안됨/Main.java
@@ -0,0 +1,92 @@
+package brute_force._15650_오름차순이며_중복허용이안되고_앞뒤바꾼숫자도_허용안됨;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+import static java.lang.Double.parseDouble;
+import static java.lang.Integer.parseInt;
+import static java.lang.Long.parseLong;
+import static java.lang.System.in;
+
+public class Main {
+    static int N, M;
+    static int[] selected;
+
+    static StringBuilder sb = new StringBuilder();
+
+    static void input() {
+        FastReader scan = new FastReader();
+        N = scan.nextInt();
+        M = scan.nextInt();
+        selected = new int[M + 1];
+    }
+
+    public static void main(String[] args) {
+        input();
+
+        rec_func(1);
+        System.out.println(sb.toString());
+    }
+
+    static void rec_func(int k) {
+        if (k == M + 1) {
+            for (int i = 1; i <= M; i++) {
+                sb.append(selected[i]).append(' ');
+            }
+            sb.append('\n');
+        } else {
+            for (int cand = selected[k - 1] + 1; cand <= N; cand++) {
+                selected[k] = cand;
+                rec_func(k + 1);
+                selected[k] = 0;
+            }
+        }
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return parseInt(next());
+        }
+
+        long nextLong() {
+            return parseLong(next());
+        }
+
+        double nextDouble() {
+            return parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+}
